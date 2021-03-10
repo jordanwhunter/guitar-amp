ef2110ef5176a370ddbb95038d59a7163694097e
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-10 15:49:52 -0500

    Imported Canvas component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Mixer from './components/Mixer';
+import Canvas from './components/Canvas';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Mixer />      
+      <Mixer />
+      <Canvas />   
     </div>
   );
 }
