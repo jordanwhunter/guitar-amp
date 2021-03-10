40d1c125b1974dcdb2cf3a86d97090a70f625def
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-10 15:24:28 -0500

    Imported Mixer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Mixer from './components/Mixer';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Mixer />      
     </div>
   );
 }
