41e873aceebe38ca6f7ecac71dd3f89e4e0c9bde
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-10 13:52:58 -0500

    Created Header component and imported style sheet

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// Dependencies
+import React from 'react';
+
+// Styles
+import '../styles/Header.css';
+
+export default function Header() {
+  return (
+    <div>
+      Hello world. I am the Header component.
+    </div>
+  )
+};
