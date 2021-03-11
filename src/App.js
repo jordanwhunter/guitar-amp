0ae16c207b6b099805f89c2394d8fd00e435a81d
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-11 14:57:10 -0500

    Removed Canvas component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Mixer from './components/Mixer';
-import Canvas from './components/Canvas';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Mixer />
-      <Canvas />   
+      <Mixer />  
     </div>
-  );
-}
+  )
+};
 
 export default App;
